7779bf1f3044129a23e085829fd5cdcbbe770153
AirtonSilva <airtonsilva268@gmail.com>
2016-03-19 22:13:09 -0300

    Exercício QRCode

---
+++ src/br/edu/ifpb/qrcode/MainActivity.java
@@ -0,0 +1,48 @@
+package br.edu.ifpb.qrcode;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        new IntentIntegrator(this).initiateScan();
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        switch (requestCode) {
+        
+            case IntentIntegrator.REQUEST_CODE:
+                IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+                
+                final String result = scanResult.getContents();
+                
+                if ((result != null)) {
+                	
+                	Toast.makeText(this, "Código: " + result, Toast.LENGTH_LONG).show();
+                	
+                	TextView textResult = (TextView) findViewById(R.id.textResultado);
+                    textResult.setText(result);
+                	
+                } else {
+                	
+                	Toast.makeText(getBaseContext(), "Código inválido ou inexistente.", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+}
+
